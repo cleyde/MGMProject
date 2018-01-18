2c9b6a241a8747d8e6f97fd5d64ee025e13fb19e
Cleyde Marlyse de Andrade <cleyde.andrade@gmail.com>
2018-01-18 11:39:00 -0200

    Add files via upload
    app_start

---
+++ ProjetoMGN.MVC/App_Start/AutoMapperConfig.vb
@@ -0,0 +1,13 @@
+﻿Imports AutoMapper
+Public Class AutoMapperConfig
+
+    Public Sub RegisterMapping()
+        Dim config As New Configuration.MapperConfigurationExpression : With config
+            .AddProfile(Of DomainToViewModelMappingProfile)()
+            .AddProfile(Of ViewModelToDomainMappingProfile)()
+        End With
+        Mapper.Initialize(config)
+
+    End Sub
+
+End Class
